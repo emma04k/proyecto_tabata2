21bcf823a1e80d1ef96f70d37c8d8f0994bf4273
emma04k <emaortegag16@gmail.com>
2019-05-05 12:04:04 -0500

    consulta terminada

---
+++ model/tabata.php
@@ -0,0 +1,16 @@
+<?php
+include_once 'conexion.php';
+
+class Tabata extends DB
+{
+
+    public static function obtenerTabta($id)
+    {
+        $query = $this->connect()->prepare('select .id as tabataID,t.nombre as tabataNombre from usuario inner join tabata t on usuario.id = t.idUsuario
+                                            where usuario.id = :id');
+        $query->execute(['id'=>$id]);
+        return $query->fetch();
+    }
+
+
+}
