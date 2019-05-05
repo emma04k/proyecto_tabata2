17b887f9a08d1cbbab562d191bd9efebfaa14e42
emma04k <emaortegag16@gmail.com>
2019-05-05 12:19:28 -0500

    Update tabata.php

--- model/tabata.php
+++ model/tabata.php
@@ -6,7 +6,7 @@ class Tabata extends DB
 
     public static function obtenerTabta($id)
     {
-        $query = $this->connect()->prepare('select .id as tabataID,t.nombre as tabataNombre from usuario inner join tabata t on usuario.id = t.idUsuario
+        $query = self::connect()->prepare('select .id as tabataID,t.nombre as tabataNombre from usuario inner join tabata t on usuario.id = t.idUsuario
                                             where usuario.id = :id');
         $query->execute(['id'=>$id]);
         return $query->fetch();
